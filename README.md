c500e84dd5f854cde04efb2947195a55813aede7
chaehyun_oh <ohchaehh@gmail.com>
2023-05-18 23:30:02 +0900

    230518 update

--- README.md
+++ README.md
@@ -10,3 +10,4 @@
 | [Day 4](./markdown/230515.md) | S.2 Basics & Core Concepts - DOM Interaction with Vue (15~20) |
 | [Day 5](./markdown/230516.md) | S.2 Basics & Core Concepts - DOM Interaction with Vue (21~24) |
 | [Day 6](./markdown/230517.md) | S.2 Basics & Core Concepts - DOM Interaction with Vue (25~28) |
+| [Day 7](./markdown/230518.md) | S.2 Basics & Core Concepts - DOM Interaction with Vue (29~31) |
--- s.2/events/app.js
+++ s.2/events/app.js
@@ -3,16 +3,40 @@ const app = Vue.createApp({
     return {
       counter: 10,
       name: '',
-      confirmedName: ''
+      confirmedName: '',
+      // fullname: '',
+      lastName: ''
     };
   },
-  methods: {
-    outputFullname() {
-      if (this.name === '') {
+  watch: {
+    counter(value) {
+      if (value > 50) {
+        this.counter = 0;
+      }
+    }
+    // name(value) {
+    //   if (value === '') {
+    //     this.fullname = ''
+    //   } else {
+    //     this.fullname = value + ' ' + 'last name';
+    //   }
+    // }
+  },
+  computed: {
+    fullname() {
+      if (this.name === '' || this.lastName === '') {
         return '';
       }
-      return this.name + ' ' + 'Last name';
-    },
+      return this.name + ' ' + this.lastName;
+    }
+  },
+  methods: {
+    // outputFullname() {
+    //   if (this.name === '') {
+    //     return '';
+    //   }
+    //   return this.name + ' ' + 'Last name';
+    // },
     confirmInput() {
       this.confirmedName = this.name;
     },
