caa67f2a4ff04c563ef278b923b5b9175f6e596d
chaehyun_oh <ohchaehh@gmail.com>
2023-07-16 23:59:48 +0900

    230716 update

--- main-prj-01-starting-setup/src/pages/requests/RequestsReceived.vue
+++ main-prj-01-starting-setup/src/pages/requests/RequestsReceived.vue
@@ -1,1 +1,9 @@
-<template>REquests</template>
+<template>
+  <section>
+    <base-card>
+      <header>
+        <h2>request received</h2>
+      </header>
+    </base-card>
+  </section>
+</template>
